73e0e5f9e539cae8f8d2644114082d1ee6ddf0d9
madinabolat <madinabolat01@gmail.com>
2026-03-04 22:45:02 -0500

    set up sockets repo

---
+++ sockets/src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+//        Sockets, Server-Client, communication protocol -
+//        bytes, data transfer back and worth
+//        Serialize objects
+//        Send a random matrix to the server and send the sum of all elements back.
+
+
+    }
+}
