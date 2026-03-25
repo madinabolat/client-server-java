397d658f9455c2aa24e56e2387373e62ff433c36
madinabolat <madinabolat01@gmail.com>
2026-03-24 20:47:04 -0400

    fix: update deprecated BottomAppBarTheme to BottomAppBarThemeData

--- sockets/src/main/java/org/example/Client.java
+++ sockets/src/main/java/org/example/Client.java
@@ -2,6 +2,8 @@ package org.example;
 
 import java.io.DataOutputStream;
 import java.net.Socket;
+import java.sql.SQLOutput;
+import java.util.Random;
 
 public class Client {
     private static final int PORT = 1234;
@@ -9,11 +11,29 @@ public class Client {
         try {
             Socket clientSocket = new Socket("localhost", PORT);
             DataOutputStream dataOutputStream = new DataOutputStream(clientSocket.getOutputStream());
-            dataOutputStream.writeUTF("hello from client!");
-            dataOutputStream.writeInt(1);
-            dataOutputStream.writeInt(2);
-            //send matrix 1
-            //send matrix 2
+//            dataOutputStream.writeUTF("hello from client!");
+//            dataOutputStream.writeInt(1);
+//            dataOutputStream.writeInt(2);
+
+            Random random = new Random();
+            int n = 3;
+            int m = 3;
+            int[][] matrix = new int[n][m];
+
+            for (int i = 0; i < n; i++){
+                for (int j = 0; j < m; j++){
+                    matrix[i][j] = random.nextInt(100);
+                }
+            }
+//
+//            for (int i = 0; i < n; i++){
+//                for (int j = 0; j < m; j++){
+//                    System.out.print(matrix[i][j]+" ");
+//                }
+//                System.out.println();
+//            }
+
+
             dataOutputStream.flush();
             dataOutputStream.close();
 
